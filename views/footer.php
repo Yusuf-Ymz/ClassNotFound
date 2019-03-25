bfd20c9a1458085b426b8cd93b3bf6ffec86b97f
yusuf.yilmaz@student.vinci.be <yusuf.yilmaz@student.vinci.be>
2019-03-25 13:29:48 +0100

    Modification footer

--- views/footer.php
+++ views/footer.php
@@ -1,28 +1,25 @@
 </div>
-<footer id="footer" class="footer-page font-small indigo bg-light">
+<footer id="footer" class="footer-page font-small indigo bg-light py-4">
     <div class="container-fluid text-center text-md-center">
         <div class="row">
             <div class="col-6 mx-auto">
-                <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Authors</h5>
+                <h6 class="font-weight-bold text-uppercase ">Authors</h6>
                 <ul class="list-unstyled">
                     <li>
                         <a href="#!">antoine.honinckx@student.vinci.be</a>
                     </li>
-                    <li>
+                    <li class="mt-1">
                         <a href="#!">yusuf.yilmaz@student.vinci.be</a>
                     </li>
                 </ul>
             </div>
             <div class="col-6 mx-auto">
-                <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Projet Web</h5>
+                <h6 class="font-weight-bold text-uppercase">Projet Web</h6>
                 <ul class="list-unstyled">
-                    <li>
-                        <p>HE - VINCI</p>
-                    </li>
-                    <li>
-                        <p>2018-2019</p>
-                    </li>
+                    <li >HE - VINCI </li>
+                    <li class="mt-1">2018-2019</li>
                 </ul>
+
             </div>
         </div>
     </div>
