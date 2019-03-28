fbce76fe3790d3ba0813e5d6b4d3bf60f4d2bd33
antoine.honinckx@student.vinci.be <antoine.honinckx@student.vinci.be>
2019-03-28 09:21:20 +0100

    commit class DB - pattern singleton

--- models/Db.php
+++ models/Db.php
@@ -2,5 +2,33 @@
 
 class Db
 {
+    private static $instance = null;
+    private $_db;
+
+    private function __construct()
+    {
+        try {
+            $this->_db = new PDO('mysql:host=localhost;dbname=projet;charset=utf8', 'root', '');
+            $this->_db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            $this->_db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+        }
+        catch (PDOException $e) {
+            die('Error: connexion to database failed : '.$e->getMessage());
+        }
+    }
+
+    # Pattern Singleton
+    public static function getInstance()
+    {
+        if (is_null(self::$instance)) {
+            self::$instance = new Db();
+        }
+        return self::$instance;
+    }
+
+    # Database scripts
+
+
+
 
 }
