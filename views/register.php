f2d98fecb5919bc8959a0c5679652520ef82ee52
HONINCKX Antoine <antoine.honinckx@student.vinci.be>
2019-03-28 15:16:41 +0100

    commit register form

--- views/register.php
+++ views/register.php
@@ -1,1 +1,27 @@
-<?php
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h3>Register Form</h3>
+            <form action="index.php?action=register" method="post">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Your First Name" name="firstname"/>
+                </div>
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Your Last Name" name="lastname"/>
+                </div>
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Your Mail" name="mail"/>
+                </div>
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Your Username" name="login"/>
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" placeholder="Your Password" name="password"/>
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-dark" type="submit" name="form_register">Register</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
