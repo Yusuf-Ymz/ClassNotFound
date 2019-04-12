bfd80d0be3af315c8d28db24dfee5a616374823b
yilmaz_yusuf <yusuf.yilmaz@student.vinci.be>
2019-04-12 16:27:00 +0200

    Adding comments at the newQuestion view

--- views/newQuestion.php
+++ views/newQuestion.php
@@ -1,12 +1,16 @@
+<!-- New Question Form -->
 <div class="container">
     <h3>New Question:</h3>
     <form action="index.php?action=newQuestion" method="post">
+        <!-- The title-->
         <div class="form-group">
             <input type="text" class="form-control" placeholder="Your title" name="question_title"/>
         </div>
+        <!-- The Subject-->
         <div class="form-group">
             <textarea class="form-control" name="question_text" rows="5" placeholder="Your Question"></textarea>
         </div>
+        <!-- Displaying all categories -->
         <div class="form-group">
             <select class="browser-default custom-select custom-select-md mb-3" name="question_category_id">
                 <option selected
@@ -16,6 +20,7 @@
                 <?php } ?>
             </select>
         </div>
+        <!-- If the form is incomplete-->
         <?php if (isset($notification)) { ?>
             <p id="notification"> <?php echo $notification; ?> </p>
         <?php } ?>
