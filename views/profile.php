3167cc45051cf5442f045861f112570007f24392
Yusuf <yusuf.yilmaz@student.vinci.be>
2019-04-23 13:57:35 +0200

    Modified profile

--- views/profile.php
+++ views/profile.php
@@ -3,25 +3,23 @@
 </div>
 <!-- Displaying all questions -->
 <?php for ($i = 0; $i < count($memberQuestions); $i++) { ?>
-    <div class="container">
-        <div class="card">
-            <div class="card-body">
-                    <!-- Searching the question's category to display -->
-                    <span class="font-weight-bold categoryQuestions pagination justify-content-end"">
-                    <?php echo $categories[$i]->name(); ?>
-                    </span>
+    <div class="card">
+        <div class="card-body">
+            <!-- Searching the question's category to display -->
+            <span class="font-weight-bold categoryQuestions ">
+            <?php echo $categories[$i]->name(); ?>
+            </span>
 
-                    <!-- Displaying the title of the question-->
-                    <a href="index.php?action=question&id=<?php echo $questions[$i]->questionId(); ?>"
-                       class="list-group-item questions">
-                        <?php echo $questions[$i]->html_title() ?>
-                    </a>
-                <!-- Displaying the question's publication date -->
-                <span class="card-deco pagination justify-content-end">
+            <!-- Displaying the title of the question-->
+            <a href="index.php?action=question&id=<?php echo $questions[$i]->questionId(); ?>"
+               class="list-group-item questions">
+                <?php echo $questions[$i]->html_title() ?>
+            </a>
+            <!-- Displaying the question's publication date -->
+            <span class="card-deco pagination justify-content-end">
                     <?php echo $questions[$i]->publicationDate() ?>
                 </span>
 
-            </div>
         </div>
     </div>
 <?php } ?>
