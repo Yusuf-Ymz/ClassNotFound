08a93e9fdbbcd3d7896e3a344c24042e1439cedf
Yusuf <yusuf.yilmaz@student.vinci.be>
2019-04-27 23:12:15 +0200

    Fix modifying questions' state

--- controllers/OpenQuestionController.php
+++ controllers/OpenQuestionController.php
@@ -18,31 +18,28 @@ class OpenQuestionController
             die();
         }
 
+        if (isset($_POST['state'])) {
+            # Setting the question as open
+            if (isset($_POST['delete_best_answer'])) {
+                $this->_db->change_question_state($_POST['question_id'], 'solved');
+            } else {
+                $this->_db->change_question_state($_POST['question_id'], null);
+            }
+            header('Location: index.php?action=question&id=' . $_POST['question_id']);
+            die();
+        }
+
         # Selecting the question
         $question = $this->_db->select_question($_POST['question_id']);
 
         # If the question is duplicated and user clicked on like or dislike
         if ($question->state() == 'duplicated') {
             $_SESSION['error'] = 'This question is marked as duplicated';
-            header('Location: index.php?action=question&id=' . $question->questionId());
-            die();
-        }
-        
-        if(isset($_POST['delete_best_answer'])){
+
+        } else {
             $this->_db->delete_best_answer($_POST['question_id']);
-            header('Location: index.php?action=question&id='.$_POST['question_id']);
-            die();
         }
-
-        if(isset($_POST['has_best_answer'])){
-            $this->_db->change_question_state($_POST['question_id'],'solved');
-            header('Location: index.php?action=question&id='.$_POST['question_id']);
-            die();
-        }
-
-        # Setting the question as open
-        $this->_db->change_question_state($_POST['question_id'],null);
-        header('Location: index.php?action=question&id='.$_POST['question_id']);
+        header('Location: index.php?action=question&id=' . $_POST['question_id']);
         die();
     }
 }
