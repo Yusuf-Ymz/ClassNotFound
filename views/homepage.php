9c677590129b107daaf02325f33f8a2791155683
Yusuf <yusuf.yilmaz@student.vinci.be>
2019-05-05 19:07:11 +0200

    added homepage notification

--- views/homepage.php
+++ views/homepage.php
@@ -1,6 +1,7 @@
-    <div class="container">
-        <h3>Newest Questions:</h3>
-    </div>
+<div class="container">
+    <h3>Newest Questions:</h3>
+</div>
+<?php if ($nbQuestions == 0) echo '<p id="notification" class="container"><i class="fas fa-exclamation-triangle"></i> No open questions have been found.</p>' ?>
 <?php for ($i = 0; $i < $nbQuestions; $i++) { ?>
     <div class="container">
         <div class="card">
@@ -8,13 +9,13 @@
 
                 <div class="row card-deco">
 
-                <!-- Here we are searching the author's firstname to display it -->
-                <span class="col-6 font-weight-bold">
-                    <?php echo $questions[$i]->author()->html_login().' asks:' ?>
+                    <!-- Here we are searching the author's firstname to display it -->
+                    <span class="col-6 font-weight-bold">
+                    <?php echo $questions[$i]->author()->html_login() . ' asks:' ?>
                 </span>
 
-                <!-- Searching the question's category to display as well-->
-                <span class="col-6 font-weight-bold categoryQuestions pagination justify-content-end">
+                    <!-- Searching the question's category to display as well-->
+                    <span class="col-6 font-weight-bold categoryQuestions pagination justify-content-end">
                     <?php echo $questions[$i]->category()->name(); ?>
                 </span>
 
