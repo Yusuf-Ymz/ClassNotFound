eec2489ceb91652b5b17021d3176a8ea1fd90608
Yusuf <yusuf.yilmaz@student.vinci.be>
2019-05-15 13:45:08 +0200

    fixes

--- controllers/HomepageController.php
+++ controllers/HomepageController.php
@@ -25,12 +25,12 @@ class HomepageController
         elseif (isset($_GET['category_id'])) {
             # Selecting the category
             $category = $this->_db->select_category($_GET['category_id']);
-            $pageTitle = $category->name() . " Questions:";
             # If the category doesn't exist redirecting to homepage
             if ($category == null) {
                 header('Location: index.php');
                 die();
             }
+            $pageTitle = $category->name() . " Questions:";
             # Select all questions + their respective author from the category with the specified id
             $questions = $this->_db->select_questions_for_category($_GET['category_id']);
 
